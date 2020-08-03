bd9dbfdb7f6259e98e88ca36923beedfee7fccd7
linxianxi <904492381@qq.com>
2020-08-03 16:26:18 +0800

    fix: 修复弹出 toast 时 Frame 占据页面高度

--- src/utils/toast.tsx
+++ src/utils/toast.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 
 export function toast(props: Omit<ToastProps, "onDismiss">): void {
   const container = document.createElement("div");
+  container.style.display = "none";
   document.body.appendChild(container);
 
   ReactDOM.render(
