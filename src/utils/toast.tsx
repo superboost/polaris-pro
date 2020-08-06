cd375e86c56012aa98e6db4e3dec63e0dafd32ff
linxianxi <904492381@qq.com>
2020-08-06 11:43:35 +0800

    fix: 修复 toast 动画

--- src/utils/toast.tsx
+++ src/utils/toast.tsx
@@ -1,6 +1,28 @@
 import { AppProvider, Frame, Toast, ToastProps } from "@shopify/polaris";
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+
+const CustomToast: FC<ToastProps> = ({ onDismiss, ...rest }) => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => setActive(true), [setActive]);
+
+  return (
+    <AppProvider i18n={{}}>
+      <Frame>
+        {active ? (
+          <Toast
+            {...rest}
+            onDismiss={() => {
+              setActive(false);
+              setTimeout(onDismiss, 1000);
+            }}
+          />
+        ) : null}
+      </Frame>
+    </AppProvider>
+  );
+};
 
 export function toast(props: Omit<ToastProps, "onDismiss">): void {
   const container = document.createElement("div");
@@ -8,17 +30,13 @@ export function toast(props: Omit<ToastProps, "onDismiss">): void {
   document.body.appendChild(container);
 
   ReactDOM.render(
-    <AppProvider i18n={{}}>
-      <Frame>
-        <Toast
-          {...props}
-          onDismiss={() => {
-            ReactDOM.unmountComponentAtNode(container);
-            document.body.removeChild(container);
-          }}
-        />
-      </Frame>
-    </AppProvider>,
+    <CustomToast
+      {...props}
+      onDismiss={() => {
+        ReactDOM.unmountComponentAtNode(container);
+        document.body.removeChild(container);
+      }}
+    />,
     container
   );
 }
